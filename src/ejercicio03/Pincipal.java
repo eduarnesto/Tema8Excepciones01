2d0d3f1520160b222f44d9df7652066a813333c9
eduarnesto <eduardo.arnesto@iesnervion.es>
2024-05-08 10:14:38 +0200

    asd

--- src/ejercicio03/Pincipal.java
+++ src/ejercicio03/Pincipal.java
@@ -3,5 +3,21 @@ package ejercicio03;
 import java.util.Scanner;
 
 public class Pincipal {
-	Scanner sc = new Scanner (System.in);
+	public static void main(String[] args) {
+		String dni;
+		String nombre;
+		int saldo;
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Introduzca el DNI");
+		dni = sc.nextLine();
+		
+		System.out.println("Introduzca el nombre");
+		nombre = sc.nextLine();
+		
+		System.out.println("Introduzca el saldo");
+		saldo = sc.nextInt();
+		sc.nextLine();
+	}
+	
 }
